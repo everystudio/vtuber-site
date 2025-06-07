f636d3c373d0a5c50c06e95c3e7a6cdd08a8e3d1
watanabe@marida <watanabe.everystudio@gmail.com>
2025-06-07 17:19:16 +0900

    設定ファイル変更

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    "flex",
+    "flex-wrap",
+    "gap-4",
+    "space-x-2",
+    "items-center"
+  ],
   theme: {
     extend: {},
   },
